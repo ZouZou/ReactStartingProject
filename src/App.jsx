a358a99a998d888254698ba91f0dbd8e12033157
Joseph Jabbour <jabbour.joseph@gmail.com>
2024-09-16 14:06:08 +0300

    using a random function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+// Important:
+// For this project to work on CodeSandbox, image assets ("assets") folder
+// must be stored in the public folder (as it's the case by default in this project)
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
-  )
+  );
 }
 
 function App() {
