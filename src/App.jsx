63d7bf7cbc967b7ac65412beee86b731426524bc
Joseph Jabbour <j.jabbour@dq.com.lb>
2024-09-20 12:20:32 +0300

    created a new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput.jsx";
-import Results from "./components/Results.jsx";
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  const inputIsValid = userInput.duration >= 1;
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: +newValue,
-      };
-    });
-  }
-
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput userInput={userInput} onChange={handleChange} />
-      {!inputIsValid && (
-        <p className="center">Please enter a duration greater than zero.</p>
-      )}
-      {inputIsValid && <Results input={userInput} />}
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
